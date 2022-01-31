f8f293690b2abf8cf0f4642e3c90d3bf69b6b313
mckalea <anthony.mckale@riversafe.co.uk>
2022-01-31 21:11:50 +0000

    AB#990: updating to new dev environment

---
+++ demos/password-example/src/main.js
@@ -0,0 +1,4 @@
+var SERVER_LOCATION = "https://username:password@localhost:1234"
+var JWT_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+
+console.log('the password is = ' + SERVER_LOCATION + JWT_TOKEN)
---
+++ demos/sql-example/run.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -x
+
+echo "Eze License Demo"
+echo "##############################################"
+echo ""
+docker run --rm -t -v $(pwd -W):/data eze-cli test --debug
